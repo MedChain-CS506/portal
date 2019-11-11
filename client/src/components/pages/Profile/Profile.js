781fa61eb8f72461117831dce8d4b82ed8037786
Brennan Fife <brenfife@Brennans-MacBook-Pro.local>
2019-11-11 15:06:23 -0600

    profile test

--- client/src/components/pages/Profile/Profile.js
+++ client/src/components/pages/Profile/Profile.js
@@ -1,72 +1,65 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import PatientContext from '../../../context/patient/PatientContext';
 
 //* MUI
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-  card: {
-    maxWidth: 690,
-    minWidth: 690,
-  },
+const useStyles = makeStyles(theme => ({
   paper: {
     m: 'auto',
+    padding: theme.spacing(3, 2),
   },
-});
+}));
 
 const Profile = () => {
   const classes = useStyles();
+
+  const patientContext = useContext(PatientContext)
+
+  useEffect(() => {
+    //getPatient()
+  }, [])
+
+  //! test variables
+  const firstName = "Satoshi"
+  const lastName = "Nakamoto"
+  const aadhar = "000011112222"
+  const dob = 1 / 1 / 2000
+  const sex = 'Male'
+  const weight = 100
+
   return (
     <>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography variant="h6" gutterBottom>
-              Patient Profile
-            </Typography>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Typography variant="h2">Patient Name</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h5">Aadhaar #</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h5">Age</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h6">Sex</Typography>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Date of Birth</Typography>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6">Weight</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h5">Known Allergies</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="h5">
-                  Recent Activity - Upcoming Appointments, Medical Operations
-                </Typography>
-              </Grid>
+      <Grid container spacing={0} direction="column" alignItems="center" justify="center">
+        <Paper className={classes.paper}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Typography variant="h2">{firstName} {lastName}</Typography>
             </Grid>
-          </CardContent>
-        </Card>
+            <Grid item xs={12}>
+              <Typography variant="h5">Aadhaar - {aadhar}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">Date of Birth - {dob}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Sex - {sex}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Weight - {weight}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">Known Allergies</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">Known Diseases</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
     </>
   );
