917f598c332e178a87f001a754f659338513639f
Brennan Fife <brenfife@Brennans-MacBook-Pro.local>
2019-11-13 17:47:11 -0600

    recommitted

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ import NotFound from './components/pages/NotFound';
 import PatientState from './context/patient/PatientState'
 
 //* Blockchain
-// import getWeb3 from './utils/getWeb3.js';
-// import MedChainContract from './contracts/med_chain.json';
+import getWeb3 from './utils/getWeb3.js';
+import MedChainContract from './contracts/med_chain.json';
 
 //* Styles
 import './index.css'
@@ -32,44 +32,44 @@ function App() {
     contract: null
   });
 
-  // useEffect(() => {
-  //   async function connectMetamask() {
-  //     try {
-  //       const web3 = await getWeb3();
-  //       const accounts = await web3.eth.getAccounts();
-  //       const networkId = await web3.eth.net.getId();
-  //       const deployedNetwork = MedChainContract.networks[networkId];
-  //       const instance = new web3.eth.Contract(
-  //         MedChainContract.abi,
-  //         deployedNetwork && deployedNetwork.address
-  //       );
-  //       let data = {
-  //         accounts: accounts,
-  //         web3: web3,
-  //         contract: instance
-  //       };
-  //       setContract(data);
-  //       setSignedIn(true);
-  //       setReady(true);
-  //       return data;
-  //     } catch (error) {
-  //       setSignedIn(false);
-  //       setReady(false);
-  //       console.error(error);
-  //     }
-  //   }
+  useEffect(() => {
+    async function connectMetamask() {
+      try {
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = MedChainContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          MedChainContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        let data = {
+          accounts: accounts,
+          web3: web3,
+          contract: instance
+        };
+        setContract(data);
+        setSignedIn(true);
+        setReady(true);
+        return data;
+      } catch (error) {
+        setSignedIn(false);
+        setReady(false);
+        console.error(error);
+      }
+    }
 
-  //   connectMetamask().then((data) => {
-  //     setInterval(async () => {
-  //       const rn = await data.web3.eth.getAccounts();
-  //       if (rn[0] !== data.accounts[0]) {
-  //         setSignedIn(false);
-  //       } else if (rn[0] === data.accounts[0]) {
-  //         setSignedIn(true);
-  //       }
-  //     }, 100)
-  //   });
-  // }, [signedIn]);
+    connectMetamask().then((data) => {
+      setInterval(async () => {
+        const rn = await data.web3.eth.getAccounts();
+        if (rn[0] !== data.accounts[0]) {
+          setSignedIn(false);
+        } else if (rn[0] === data.accounts[0]) {
+          setSignedIn(true);
+        }
+      }, 100)
+    });
+  }, [signedIn]);
 
   //! To explain this return...
   //! First, wrap our components with the provided PatientState (i.e. Model, which can be found in the context folder)
@@ -83,8 +83,8 @@ function App() {
           <Navbar />
           <div className='container'>
             <Switch>
-              {/* <Route exact path="/" render={(props) => <Landing {...props} signedIn={signedIn} contract={contract} />} /> */}
               <Route exact path="/" render={(props) => <Landing {...props} signedIn={signedIn} contract={contract} />} />
+              {/* <Route exact path="/" render={(props) => <Landing {...props} signedIn={signedIn} contract={contract} />} /> */}
               <Route exact path="/patient-form" render={(props) => <PatientForm {...props} signedIn={signedIn} contract={contract} />} />
               <Route exact path="/profile/:id" render={(props) => <Profile {...props} signedIn={signedIn} />} />
               <Route component={NotFound} />
